4de4ce2ea4f3c71208bdc4113e221861d463f750
Jacob Radford <jradford@hunter.cuny.edu>
2009-10-22 14:32:02 -0400

    bump dev version

--- src/fakeldap/setup.py
+++ src/fakeldap/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "fakeldap",
-    version = "1.4",
+    version = "1.5",
     url = 'http://github.com/hcwebdev/fakeldap',
     license = '',
     description = "Fake LDAP Tools",
